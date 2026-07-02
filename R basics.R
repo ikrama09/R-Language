ab260ad279e722be3e5fee7afb09bc3e2da047ca
Ikrama Kathiyara <144513088+ikrama09@users.noreply.github.com>
2026-07-02 20:25:09 +0530

    Add files via upload
    R basics (print function ,data types ,variables ,sum function ,type of function.

---
+++ R basics.R
@@ -0,0 +1,47 @@
+'1. define Variables in R'
+name <- "Ikrama"
+age <- 20
+salary <- 25000
+print(name)
+print(age)
+print(salary)
+
+'2. Print function in R language'
+print("Hello, World!")
+message <- "Welcome to R programming"
+print(message)
+
+'3. Data Types in R programming'
+
+#3.1 Numeric
+num <- 100
+
+#3.2 String(charcter)
+text <- "R language is awesome"
+
+#3.3 Logical
+isPassed <- TRUE
+
+print(num)
+print(text)
+print(isPassed)
+
+'4 sum function in R'
+a <- 10
+b <- 20
+result <- a + b
+print(result)
+
+#Example of sums
+total <- sum(10,20,30,40)
+print(total)
+
+'5. Find data type in R'
+x <- 50
+y <- "i am String"
+z <- FALSE
+
+print(typeof(x))
+print(typeof(y))
+print(typeof(z))
+
