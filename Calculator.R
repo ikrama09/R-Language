165a460813eec97cff70fe426cf554e337a35a34
Ikrama Kathiyara <144513088+ikrama09@users.noreply.github.com>
2026-07-08 10:10:38 +0530

    calculator in R using basic operators.

---
+++ Calculator.R
@@ -0,0 +1,24 @@
+#calculator
+#without objects.
+10+20
+50-25
+40*10
+500/10
+print(50+50)
+
+#with objects
+var1 <- 10
+var2 <-20
+var1+var2
+
+sub1=25
+sub2=10
+sub1-sub2
+
+#syllabus practice 2
+day1 <- 32
+day2 <- 34
+day3 <- 30
+aveage_temp = (day1+day2+day3)/3
+aveage_temp
+
